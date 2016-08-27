8a23f25f2e60ee2f14fc3fae6ad01eacca312cbc
Kenneth Korcal <kakorcal@gmail.com>
2016-08-26 19:25:36 -0700

    adding panels

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,11 @@
 import React, {Component} from 'react'
-import thumbnail from '../assets/thumbnail.png'
-import resume from '../assets/Kenneth_Korcal_Resume.pdf'
+import Nav from './Nav'
 
 export default class App extends Component{
   render(){
     return (
       <div className='k-wrapper'>
-        <div className='k-nav'>
-          <div className='k-about'>
-            <h3>Thanks For Stopping By!</h3>
-            <img src={thumbnail} height="215" width="215" alt="pic" />    
-            <p>Hi, my name is Kenneth Korcal. I am a Front-End Web Developer based in San Francisco, California. You can check out my projects here. Have a nice day!</p>    
-          </div>
-          <div className='k-contact'>
-            <div>
-              <a href='https://github.com/kakorcal' target='_blank' className='btn btn-default'>
-                <i className='fa fa-github'></i> <span>GITHUB</span>
-              </a>
-            </div>
-            <div>
-              <a href='https://www.linkedin.com/in/kennethkorcal' target='_blank' className='btn btn-default'>
-                <i className='fa fa-linkedin'></i> <span>LINKEDIN</span>
-              </a>   
-            </div>
-            <div>
-              <a href={resume} target='_blank' className='btn btn-default'>
-                <i className='fa fa-file-text-o'></i> <span>RESUME</span>
-              </a>
-            </div>
-          </div>
-        </div>
+        <Nav></Nav>
         <div className='k-body'>
           {this.props.children}
         </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,25 @@ import {Link} from 'react-router'
 export default class Home extends Component {
   render(){
     return (
-      <div>
-        <h3>Home</h3>
-        <div><Link to='/linguo'>Linguo</Link></div>
+      <div className='col k-posts'>
+        <div className='col col-lg-4 col-md-6 k-post'>
+          <Link to='/linguo' className='k-box jump-in k-1'>1</Link>
+        </div>
+        <div className='col col-lg-4 col-md-6 k-post'>
+          <Link to='/linguo' className='k-box jump-in k-2'>2</Link>
+        </div>
+        <div className='col col-lg-4 col-md-6 k-post'>
+          <Link to='/linguo' className='k-box jump-in k-3'>3</Link>
+        </div>
+        <div className='col col-lg-4 col-md-6 k-post'>
+          <Link to='/linguo' className='k-box jump-in k-4'>4</Link>
+        </div>
+        <div className='col col-lg-4 col-md-6 k-post'>
+          <Link to='/linguo' className='k-box jump-in k-5'>5</Link>
+        </div>
+        <div className='col col-lg-4 col-md-6 k-post'>
+          <Link to='/linguo' className='k-box jump-in k-6'>6</Link>
+        </div>          
       </div>
     );
   }
