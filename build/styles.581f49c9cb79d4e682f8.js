827aae06ce349e51d06d81115f62e5d76649d8ad
Kenneth Korcal <kakorcal@gmail.com>
2016-08-26 20:15:13 -0700

    minor animation adjustment

---
+++ build/styles.581f49c9cb79d4e682f8.js
@@ -0,0 +1,1 @@
+!function(n){function t(r){if(o[r])return o[r].exports;var e=o[r]={exports:{},id:r,loaded:!1};return n[r].call(e.exports,e,e.exports,t),e.loaded=!0,e.exports}var o={};return t.m=n,t.c=o,t.p="/build/",t(0)}({0:function(n,t,o){o(279),o(280),n.exports=o(262)},262:function(n,t){},279:function(n,t){},280:function(n,t){}});
--- build/styles.f9c02b3bb7aae1cb5cb2.js
+++
@@ -1,1 +0,0 @@
-!function(n){function t(r){if(o[r])return o[r].exports;var e=o[r]={exports:{},id:r,loaded:!1};return n[r].call(e.exports,e,e.exports,t),e.loaded=!0,e.exports}var o={};return t.m=n,t.c=o,t.p="/build/",t(0)}({0:function(n,t,o){o(278),o(279),n.exports=o(261)},261:function(n,t){},278:function(n,t){},279:function(n,t){}});
